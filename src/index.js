b3ec5996236ede001708a310ed592859df2f1c83
Pietro Lauria <ptr.lauria@gmail.com>
2021-12-29 19:19:39 +0100

    till section3 28

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const App = () => {
+  const mamma = { text: 'mimmo' };
+  return (
+    <div style={{ color: 'red' }}>
+      <p>Hi There!</p>
+      <p>{mamma.text}</p>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
